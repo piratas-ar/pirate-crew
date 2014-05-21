840d7aacec2d65ddb540738540dbd0e42d6d3bbe
Matías MI <matias.mi@riseup.net>
2014-05-20 22:44:31 -0300

    Fixes #1: send notification email to new affiliates and permanent assembly.

--- app/user.js
+++ app/user.js
@@ -1,10 +1,37 @@
-/** Creates the new user and sends a notification. */
+var path = require("path");
+var Mailer = require("../lib/Mailer");
+
+/** Creates the new user and sends a notification to the general assembly. */
 app.post("/user/create", function (req, res) {
   var userName = req.param("nickname");
   var email = req.param("email");
+  var interests = [
+    req.param("freeCulture"),
+    req.param("copyright"),
+    req.param("surveillance"),
+    req.param("commonResources"),
+    req.param("infrastructure"),
+    req.param("pairProduction"),
+    req.param("coops"),
+    req.param("education"),
+    req.param("federalism"),
+    req.param("activism")
+  ];
 
-  // Send email to notify new user.
-  console.log(userName + ":" + email);
+  var mailer = new Mailer(path.join(__dirname, "..", "views", "email_welcome"));
+
+  mailer.send(email, {
+    userName: userName,
+    interests: interests.filter(function (interest) {
+      return interest !== undefined && interest !== null;
+    }).join(", ")
+  }, function (error, responseStatus) {
+    if (error) {
+      throw error;
+    } else {
+      console.log(responseStatus);
+    }
+  });
 
   res.render("affiliate_success.html");
 });
--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 export KEY_ID=[Public-Key-Id]
 export NODE_MEMBERS_DIR=members
+export MAIL_HOST=partidopirata.com.ar
+export MAIL_PORT=587
+export MAIL_USER=
+export MAIL_PASSWD=
+export MAIL_FROM=afiliaciones@partidopirata.com.ar
+#export MAIL_CC=general@asambleas.partidopirata.com.ar
 
 node index.js
